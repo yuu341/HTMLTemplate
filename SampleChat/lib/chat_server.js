9f55eaf4cecbf8c4d4ab394c1fc0664e1215a65d
yusuke furuya <y-furuya@kistem.co.jp>
2016-04-12 17:14:24 +0900

    サンプルソースコードより

--- SampleChat/lib/chat_server.js
+++ SampleChat/lib/chat_server.js
@@ -1,69 +1,52 @@
 var socketio = require('socket.io');
 var io;
 var guestNumber = 1;
-var nickNames = ;
+var nickNames = {};
 var namesUsed = [];
-var currentRoom = ;
+var currentRoom = {};
 
-export = function(server){
-
+exports.listen = function(server) {
   io = socketio.listen(server);
-
-  io.set('log level' , 1);
-
-  io.sockets.on('connection' , function(socket){
-    guestNumber = assignGuestName(socket , guestNumber, ?nickNames , namesUsed);
-
+  io.set('log level', 1);
+  io.sockets.on('connection', function (socket) {
+    guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
     joinRoom(socket, 'Lobby');
-
-    handleMessageBroadcasting(socket,nickNames);
-
-    handleNameChangeAttempts(socket, nickNames , namesUsed);
-
+    handleMessageBroadcasting(socket, nickNames);
+    handleNameChangeAttempts(socket, nickNames, namesUsed);
     handleRoomJoining(socket);
-
-    socket.on('rooms' , function(){
-      socket.emit('rooms' , io.sockets.manager.rooms);
+    socket.on('rooms', function() {
+      socket.emit('rooms', io.sockets.manager.rooms);
     });
-
-    handleClientDisconnection(socket, nickNames , namesUsed);
+    handleClientDisconnection(socket, nickNames, namesUsed);
   });
-}
+};
 
-//ゲスト名の割り当て
-function assignGuestName(socket , guestNumber , nickNames , namesUsed){
+function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
   var name = 'Guest' + guestNumber;
-
   nickNames[socket.id] = name;
-
-  socket.emit('nameResult' , { success: true , name: name});
+  socket.emit('nameResult', {
+    success: true,
+    name: name
+  });
   namesUsed.push(name);
-
   return guestNumber + 1;
 }
 
-//ルームへの参加
-function joinRoom(socket , room){
+function joinRoom(socket, room) {
   socket.join(room);
-
   currentRoom[socket.id] = room;
-
-  socket.emit('joinResult',{room:room});
-
-  socket.broadcast.to(room).emit('message',{
-    text:nickNames[socket.id] + ' has joined ' + room + '.'
+  socket.emit('joinResult', {room: room});
+  socket.broadcast.to(room).emit('message', {
+    text: nickNames[socket.id] + ' has joined ' + room + '.'
   });
 
   var usersInRoom = io.sockets.clients(room);
-
-  if(usersInRoom.length > 1){
+  if (usersInRoom.length > 1) {
     var usersInRoomSummary = 'Users currently in ' + room + ': ';
-
-    for(var index in usersInRoom){
+    for (var index in usersInRoom) {
       var userSocketId = usersInRoom[index].id;
-
-      if(userSocketId != socket.id){
-        if(index > 0){
+      if (userSocketId != socket.id) {
+        if (index > 0) {
           usersInRoomSummary += ', ';
         }
         usersInRoomSummary += nickNames[userSocketId];
@@ -74,50 +57,54 @@ function joinRoom(socket , room){
   }
 }
 
-//名前変更要求の処理
-function handleNameChangeAttempts(socket, nickNames, namesUsed){
-  socket.on('nameAttempt', function(name){
-    if(name.indexOf('Guest') == 0){
+function handleNameChangeAttempts(socket, nickNames, namesUsed) {
+  socket.on('nameAttempt', function(name) {
+    if (name.indexOf('Guest') == 0) {
       socket.emit('nameResult', {
-        success: false, message:'Names cannot begin with "Guest".'
+        success: false,
+        message: 'Names cannot begin with "Guest".'
       });
-    }else{
-      if(namesUsed.indexOf(name) == -1){
+    } else {
+      if (namesUsed.indexOf(name) == -1) {
         var previousName = nickNames[socket.id];
         var previousNameIndex = namesUsed.indexOf(previousName);
         namesUsed.push(name);
         nickNames[socket.id] = name;
         delete namesUsed[previousNameIndex];
-
-        socket.emit('nameResult' , { success: true , name: name});
-        socket.broadcast.to(currentRoom[socket.id]).emit('message' , { text: previousName + ' is now known as ' + name + '.'});
-      }else{
-        socket.emit('nameResult', { success: false , message: 'That name is already in use.'});
+        socket.emit('nameResult', {
+          success: true,
+          name: name
+        });
+        socket.broadcast.to(currentRoom[socket.id]).emit('message', {
+          text: previousName + ' is now known as ' + name + '.'
+        });
+      } else {
+        socket.emit('nameResult', {
+          success: false,
+          message: 'That name is already in use.'
+        });
       }
     }
   });
 }
 
-//チャットメッセージの送信
-function handleMessageBroadcasting(socket){
-  socket.on('message' , function(message){
-    socket.broadcast.to(message.room).emit('message' , {
+function handleMessageBroadcasting(socket) {
+  socket.on('message', function (message) {
+    socket.broadcast.to(message.room).emit('message', {
       text: nickNames[socket.id] + ': ' + message.text
     });
   });
 }
 
-//ルーム変更
-function handleRoomJoining(socket){
-  socket.on('join' , function(room){
+function handleRoomJoining(socket) {
+  socket.on('join', function(room) {
     socket.leave(currentRoom[socket.id]);
-    joinRoom(socket,room.newRoom);
+    joinRoom(socket, room.newRoom);
   });
 }
 
-//ユーザ接続断
-function handleClientDisconnection(socket){
-  socket.on('disconnect' , function(){
+function handleClientDisconnection(socket) {
+  socket.on('disconnect', function() {
     var nameIndex = namesUsed.indexOf(nickNames[socket.id]);
     delete namesUsed[nameIndex];
     delete nickNames[socket.id];
--- SampleChat/public/javascripts/chat.js
+++ SampleChat/public/javascripts/chat.js
@@ -1,37 +1,43 @@
-var Chat = function(socket){
+var Chat = function(socket) {
   this.socket = socket;
-}
+};
 
-Chat.prototype.sendMessage = function(room,text){
-  var message ={
-    room: room, text:text
+Chat.prototype.sendMessage = function(room, text) {
+  var message = {
+    room: room,
+    text: text
   };
-  this.socket.emit('message',message);
-}
+  this.socket.emit('message', message);
+};
 
-Chat.prototype.changeRoom = function(room){
-  this.socket.emit('join',{newRoom: room});
-}
+Chat.prototype.changeRoom = function(room) {
+  this.socket.emit('join', {
+    newRoom: room
+  });
+};
 
-Chat.prototype.processCommand = function(command){
+Chat.prototype.processCommand = function(command) {
   var words = command.split(' ');
-  var command = words[0].substring(1, words[0].length).toLowerCase();
+  var command = words[0]
+                .substring(1, words[0].length)
+                .toLowerCase();
   var message = false;
 
-  switch(command){
+  switch(command) {
     case 'join':
-    words.shift();
-    var room = words.join(' ');
-    this.changeRoom(room);
-    break;
+      words.shift();
+      var room = words.join(' ');
+      this.changeRoom(room);
+      break;
     case 'nick':
-    words.shift();
-    var name = words.join(' ');
-    this.socket.emit('nameAttempt' , name);
-    break;
+      words.shift();
+      var name = words.join(' ');
+      this.socket.emit('nameAttempt', name);
+      break;
     default:
-    message = 'Unrecognized command.';
-    break;
-  }
+      message = 'Unrecognized command.';
+      break;
+  };
+
   return message;
-}
+};
